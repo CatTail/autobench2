23fcf7e5c1950023cf9ba1d0bea26070aedf8198
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2019-12-09 15:30:29 +0800

    initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Autobench2
+========================
+
+HTTP benchmarking suite for `wrk2 <https://github.com/giltene/wrk2>`_.
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='autobench2',
+    version='0.1.0',
+    description='HTTP benchmarking suite for wrk2',
+    long_description=readme,
+    author='Chiyu Zhong',
+    author_email='zhongchiyu@gmail.com',
+    url='https://github.com/cattail/autobench2',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
