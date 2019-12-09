9eef777b25e3ccda491f33be67607f33515de9d0
Chiyu Zhong <zhongchiyu@gmail.com>
2019-12-09 19:27:53 +0800

    rename binary name to autobench

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     license="MIT License",
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
-        'console_scripts': ['autobench2=autobench2.cli:cli']
+        'console_scripts': ['autobench=autobench2.cli:cli']
     },
 )
