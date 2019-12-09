3310afed0ef52fae36eba7dac10539072b45254b
Chiyu Zhong <zhongchiyu@gmail.com>
2019-12-09 16:09:04 +0800

    add click deps

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setup(
     author_email='zhongchiyu@gmail.com',
     url='https://github.com/cattail/autobench2',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': ['autobench2=autobench2.cli:cli']
+    },
 )
