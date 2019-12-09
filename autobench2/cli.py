2e40aef7f9473811cf6b111f0264847411e71124
Chiyu Zhong <zhongchiyu@gmail.com>
2019-12-09 20:25:08 +0800

    construct command

--- autobench2/cli.py
+++ autobench2/cli.py
@@ -1,9 +1,13 @@
 import click
+import shutil
+import subprocess
+import sys
 
 
 @click.command()
+@click.argument('url')
 # autobench2 command line options
-@click.option('--debug', is_flag=True, default=False, help='Enable debug logging')
+@click.option('--verbose', is_flag=True, default=False, help='Print verbose messages')
 @click.option('--warmup', required=True, help='Duration of warm-up')
 @click.option('--low_rate', required=True, type=int, help='The initial rate')
 @click.option('--high_rate', required=True, type=int, help='The final rate')
@@ -21,7 +25,39 @@ import click
 @click.option('-B', '--batch_latency', is_flag=True, default=False,
               help='Measure latency of whole batches of pipelined ops (as opposed to each op)')
 @click.version_option()
-def cli(debug, **args):
-    if debug:
-        click.echo("get command line options {}".format(args))
-    click.echo('Hello World!')
+def cli(url, verbose, warmup, low_rate, high_rate, rate_step, file, **args):
+    if verbose:
+        click.echo('get command line options {}'.format(args))
+
+    if shutil.which('wrk') is None:
+        sys.exit('wrk2 not installed')
+
+    cmd = ['wrk']
+    for key, value in args.items():
+        if value is not None:
+            # skip flag option value
+            if type(value) is not bool:
+                cmd.append('--{}'.format(key))
+                cmd.append(str(value))
+            elif value is True:
+                cmd.append('--{}'.format(key))
+
+    for rate in inclusive_range(low_rate, high_rate, rate_step):
+        command = cmd.copy()
+
+        command.append('--rate')
+        command.append(str(rate))
+
+        command.append(url)
+
+        if verbose:
+            click.echo('executing command: {}'.format(' '.join(command)))
+
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        click.echo(stdout)
+        click.secho(stderr.decode("utf-8"), fg='red', err=True)
+
+
+def inclusive_range(start, stop, step):
+    return list(range(start, stop, step)) + [stop]
